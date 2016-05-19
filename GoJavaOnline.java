7b46b7852e84fa73159197d978c5373967db9942
Denys <den.ladygin@gmail.com>
2016-05-19 18:23:49 +0300

    Add files via upload

---
+++ GoJavaOnline.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Den on 5/17/16.
+ */
+public class GoJavaOnline {
+    public static void main(String[] args){
+        System.out.println("Hi, I'm your first Java program! Stay tuned!");
+    }
+}
